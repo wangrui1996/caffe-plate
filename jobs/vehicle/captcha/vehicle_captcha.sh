78a096fee4142c9202ad70fbe9a78a5cd8cbf568
2381892713@qq.com <2381892713@qq.com>
2019-05-05 14:41:20 +0800

    update warpctcNetbody

--- jobs/vehicle/captcha/vehicle_captcha.sh
+++ jobs/vehicle/captcha/vehicle_captcha.sh
@@ -1,5 +1,5 @@
 cd /home/rui/repos/warpctc-caffe
 ./build/tools/caffe train \
 --solver="models/vehicle/captcha/solver.prototxt" \
---weights="models/vehicle/captcha/vehicle_captcha_iter_4218.caffemodel" \
+--weights="models/vehicle/captcha/vehicle_captcha_iter_150127.caffemodel" \
 --gpu 0 2>&1 | tee jobs/vehicle/captcha/vehicle_captcha.log
--- python/caffe/model_libs.py
+++ python/caffe/model_libs.py
@@ -7,7 +7,7 @@ from caffe import params as P
 
 from caffe.proto import caffe_pb2
 
-def WarpctcNetBody(net, data_layer):
+def WarpctcNetBody(net, data_layer, time_step, num_classes):
 #  lstm_kwargs = {
  #   'weight_filler': dict(type='xavier'),
   #  'bias_filler': dict(type='constant', value=0)}
@@ -24,11 +24,11 @@ def WarpctcNetBody(net, data_layer):
     'weight_filler': dict(type='xavier'),
     'bias_filler': dict(type='constant', value=0)
   }
-  net.indicator = L.ContinuationIndicator(time_step=80, batch_size=512)
+  net.indicator = L.ContinuationIndicator(time_step=time_step, batch_size=512)
   net.permuted_data = L.Permute(data_layer, order=[3, 0, 1, 2])
   net.lstm1 = L.LSTM(net.permuted_data, net.indicator, recurrent_param=recurrent_param)
   net.lstm2 = L.LSTM(net.lstm1, net.indicator, recurrent_param=recurrent_param)
-  net.fc1 = L.InnerProduct(net.lstm2, num_output=11, axis=2, **kwargs)
+  net.fc1 = L.InnerProduct(net.lstm2, num_output=num_classes+1, axis=2, **kwargs)
 
 
   return net
